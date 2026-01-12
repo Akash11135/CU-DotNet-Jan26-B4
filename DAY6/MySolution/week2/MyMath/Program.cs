631861ffd6579ac00a1a85f2c4ae7d4d72567589
Akash <akashpandey1644@gmail.com>
2026-01-12 12:36:57 +0530

    day5 assessments

---
+++ DAY6/MySolution/week2/MyMath/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibrary;
+
+namespace UseLiberaryDemo
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMathFunctions math = new MyMathFunctions();
+            Console.WriteLine(math.Add(5, 10)) ;
+        }
+    }
+}
