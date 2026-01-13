13097511607cfbff8ec8be53e2e5f2532bc5cd30
Akash <akashpandey1644@gmail.com>
2026-01-13 23:19:40 +0530

    structure changes

---
+++ Assessment/DAY7/day7/day7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace day7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            Console.WriteLine("Enter the age and name : ");
+            age = int.Parse(Console.ReadLine());
+            name = Console.ReadLine();
+            if (age >= 18)
+            {
+                Console.WriteLine($"Hi {name} , you're Eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine($"Hi {name}, you're Not eligible to vote.");
+            }
+        }
+    }
+}
