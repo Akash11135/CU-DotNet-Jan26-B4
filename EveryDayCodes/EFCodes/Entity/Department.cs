edf74a26a6c220ae65d94f269d4ef304ea060051
Akash <akashpandey1644@gmail.com>
2026-03-02 12:21:57 +0530

    cleaning repo

---
+++ EveryDayCodes/EFCodes/Entity/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodes.Entity
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+        //navigation property for 1->many
+        public List<Employee> Employees {get; set;}
+    }
+}
